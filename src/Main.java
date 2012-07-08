7819a9d37bb7472c3b32f309d0b28f679ceab7c0
Nattapong Suwansukho <tehpatumwan72@gmail.com>
2012-07-09 01:28:58 +0700

    Change-Id: I818b8e7d7823cbc9cd51d45a436b55516018fafd

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		System.out.println("Dev2");
-
+		System.out.println("Dev2");
 		System.out.println("Dev1");
 
 	}
